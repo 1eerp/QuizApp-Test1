88dde31d07c6420e475d5ea47acda78c0fd7f96f
Preet <w3irdg@gmail.com>
2022-06-26 17:55:23 -0400

    testing react app

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
 const cors = require('cors');
 const express = require('express');
+const path = require('path');
 const cool = require('cool-ascii-faces');
+
 const port = process.env.PORT || 5000;
 const app = express();
 
 require('dotenv').config();
 app.use(cors())
+.use(express.static(path.join(__dirname, "/client/build")))
 .get('/', (req, res) =>{
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+})
+.get('/', (res, req)=>{
     res.send(cool());
 })
 .listen(port, ()=>{
