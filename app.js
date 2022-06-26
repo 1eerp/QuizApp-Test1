08d30f8555292a84ee0309438fc2b6528ed43908
Preet <w3irdg@gmail.com>
2022-06-26 02:50:30 -0400

    cool faces (this time for real)

--- app.js
+++ app.js
@@ -1,17 +1,14 @@
 const cors = require('cors');
 const express = require('express');
-
+const cool = require('cool-ascii-faces');
 const port = process.env.PORT || 5000;
 const app = express();
 
 require('dotenv').config();
-app.use(cors());
-
-app.get('/', (req, res) =>{
-    res.send('Hello')
+app.use(cors())
+.get('/', (req, res) =>{
+    res.send(cool());
 })
-
-
-app.listen(port, ()=>{
+.listen(port, ()=>{
     console.log('Listening on port 5000...')
-})
+});
